4feebb750e4a2af7d2b7c1f0bbccc70121770418
Ariana <arianafamiliar@hotmail.com>
2020-01-10 11:30:50 -0500

    updated main demo file

--- analyze_IC_brainnetome.m
+++ analyze_IC_brainnetome.m
@@ -6,64 +6,57 @@
 
 hemispheres = {'LH','RH'}; % 'LH' and/or 'RH'
 
-%% make sure cosmo-mvpa and IC toolboxes are on matlab path
-addpath(genpath('/Users/afam/Documents/MATLAB/CoSMoMVPA-master'));
-addpath(genpath('IC_toolbox'))
+%% add current directory and sub-dir's to path
+addpath(genpath(pwd))
 
 %% load ROI data
+disp('STATUS: loading atlas ROIs')
 load('brainnetome_ROI/LH_ROIs.mat');
 load('brainnetome_ROI/RH_ROIs.mat');
 load('brainnetome_ROI/ROI_names.mat');
 
 %% load timing info
+disp('STATUS: loading timing info')
 load('timing/selector.mat');
 load('timing/conditions.mat');
 load('timing/folds.mat');
 
 %% run IC
-numSubjs = 1;
-
 IC = [];
-IC.ROI_names = ROI_names;
+IC.ROI_names  = ROI_names;
 IC.conditions = conditions;
-IC.selector = selector;
-IC.folds = folds;
+IC.selector   = selector;
+IC.folds      = folds;
 
-for s = 1:numSubjs
-     disp(['subject ' int2str(s)])
-     % load subject data
-     if s < 10
-         data_file = ['niftiDATA_Subject00' int2str(s) '_Condition000.nii'];
-     elseif s < 100
-         data_file = ['niftiDATA_Subject0' int2str(s) '_Condition000.nii'];
-     end
-     data_path = fullfile('data/',data_file);
+% load subject data
+disp('STATUS: loading subject data')
+data_file = 'niftiDATA_Subject001.nii.gz';
+data_path = fullfile('data/',data_file);
 
-     ds = cosmo_fmri_dataset(data_path);
-     data = ds.samples';
-     
-     % update IC structure
-     IC.data = [];
-     IC.data = data;
-     
-     % run IC for each hemisphere separately
-     for h = 1:size(hemispheres,2)
-         if strcmp(hemispheres{h},'LH')
-             ROIs = LH_ROIs;
-         elseif strcmp(hemispheres{h},'RH')
-             ROIs = RH_ROIs;
-         end
-         
-         % update IC structure
-         IC.ROIs = [];
-         IC.ROIs = ROIs;
+ds = cosmo_fmri_dataset(data_path);
+data = ds.samples';
 
-         % run IC
-         [~, IC_matrix] = run_ROI_IC(IC);
+% update IC structure
+IC.data = data;
 
-         % save results
-         save(['IC_results/subj' int2str(s),'_', hemispheres{h} '.mat'],'IC_matrix')
-     end
-     
+% run IC for each hemisphere separately
+for h = 1:size(hemispheres,2)
+	if strcmp(hemispheres{h},'LH')
+        ROIs = LH_ROIs;
+    elseif strcmp(hemispheres{h},'RH')
+        ROIs = RH_ROIs;
+	end
+
+    % update IC structure
+    IC.ROIs = [];
+    IC.ROIs = ROIs;
+
+    % run IC
+    disp(['STATUS: running IC in ' hemispheres{h}])
+    [~, IC_matrix] = run_ROI_IC(IC);
+
+    % save results to mat file
+    disp('STATUS: saving results')
+    save(['IC_results/sub001_', hemispheres{h} '.mat'],'IC_matrix')
 end
 
